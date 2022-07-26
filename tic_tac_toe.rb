41c2e0e41b8bd3c7671baf23284db954e4e81533
nbrennan26 <nbrennan26@gmail.com>
2022-07-26 11:12:58 -0500

    Add game_win check, add notes for what to add still

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Main Class with most game-driving logic
 class Game
   WINNING_COMBOS = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
@@ -21,22 +22,18 @@ class Game
     @players << player_one
     player_two = @human_or_ai == 'p' ? HumanPlayer.new : ComputerPlayer.new
     @players << player_two
+    @current_player = @players[rand(2)]
   end
 
-  def change_current_player
-    # Code
+  def current_player
+    puts @current_player
+    @current_player
   end
 
-  def check_for_winner
-    # Code
-  end
-
-  def end_game
-    # Code
-  end
-
-  def access_players
-    @players
+  def player_won?(player)
+    WINNING_COMBOS.any? do |combo|
+      combo.all? { |square| @board_values[square] == player.marker }
+    end
   end
 
   # Board Methods
@@ -46,6 +43,14 @@ class Game
 
   def assign_square(square, player)
     board_values[square] = player.marker
+  end
+
+  def board_empty?
+    @board_values.all? { |square| square == ' ' }
+  end
+
+  def board_full?
+    @board_values.none? { |square| square == ' ' }
   end
 
   def display_board_keys
@@ -73,6 +78,7 @@ class Game
   end
 end
 
+# Player Superclass
 class Player
   @@player_count = 0
   def initialize
@@ -82,6 +88,10 @@ class Player
     @marker = gets.chomp[0]
     @is_ai = false
     @@player_count += 1
+  end
+
+  def claim_square(square)
+
   end
 
   # def claim_square(board, square)
@@ -95,24 +105,17 @@ class Player
   end
 end
 
+# Human Player Subclass
 class HumanPlayer < Player
   # Code
 end
 
+# Computer Player Subclass - Can select random square
 class ComputerPlayer < Player
   def initialize
     super
     @is_ai = true
   end
-
-  # def select_random_square(board)
-  #   rand_num = rand(9)
-  #   if board.is_claimed?(rand_num)
-  #     select_random_square
-  #   else
-  #     claim_square(board, rand_num)
-  #   end
-  # end
 end
 
 game = Game.new
@@ -121,6 +124,10 @@ p game.players
 
 game.display_board_keys
 game.display_current_board
+game.current_player
+
+game.board_empty?
+game.board_full?
 
 # game.access_players.each do |player|
 #   p player.give_info
@@ -128,3 +135,46 @@ game.display_current_board
 # end
 
 # board.display_current_board
+
+########
+#######
+######
+#####
+
+# Ask for square selection
+# Change Turn/current player
+# Player gets square
+# Square is already taken
+# AI picks random square
+# Check for winner (combos)
+# Check for draw (board empty, no winner)
+# End game
+# Ask for reset/new game
+
+# def change_current_player
+# def check_for_winner
+# def end_game
+
+# def select_random_square(board)
+#   rand_num = rand(9)
+#   if board.is_claimed?(rand_num)
+#     select_random_square
+#   else
+#     claim_square(board, rand_num)
+#   end
+# end
+
+# Game win logic
+# given board_values and WINNING COMBOS
+# If the board values at WINNING COMBOS are all either players marker, they win
+# for each |combo| in WINNING_COMBOS
+  # for each |value| in combo
+    # if @board_values[value]
+  #board_values[]
+
+#  WINNING_COMBOS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+#  @board_values = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+
+# def player_won?(player)
+# winning_combos.any? do |combo|
+# combo.all? { |square| @board_values[sqaure] == player.marker } 
